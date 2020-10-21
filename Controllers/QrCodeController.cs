7b311db58755fd1c7ca8ef748140c50cffa60ecf
Jhonathan Araujo <jreboucas@apoioprodesp.sp.gov.br>
2020-10-21 15:33:23 -0300

    Usando QRCoder

---
+++ Controllers/QrCodeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCode.Controls;
+
+namespace QRCode.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        [HttpPost("qrcode")]
+        public IActionResult GetQrCode(string text)
+        {
+            if (text is not null)
+            {
+                var imageT = QrCodeGenerator.GenerateByteArray(text);
+                return File(imageT, "image/jpeg"); ;
+            }
+
+            return RedirectToAction(actionName: "Index", controllerName: "Home");
+        }
+    }
+}
---
+++ Controls/QrCodeGenerator.cs
@@ -0,0 +1,34 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace QRCode.Controls
+{
+    public static class QrCodeGenerator
+    {
+        public static byte[] GenerateByteArray(string text)
+        {
+            var image = GenerateImage(text);
+            return ImageToByte(image);
+        }
+
+        private static Bitmap GenerateImage(string text)
+        {
+            using var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCoder.QRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(10);
+
+            return qrCodeImage;
+        }
+
+        private static byte[] ImageToByte(Image image)
+        {
+            using var stream = new MemoryStream();
+            image.Save(stream, ImageFormat.Png);
+            
+            return stream.ToArray();
+        }
+    }
+}
